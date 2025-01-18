317747b98552ea4fc4a7efa7c29572b0c27a8469
Gabriel.Almeida-10 <105819653+Gabriel-Almeida0@users.noreply.github.com>
2025-01-17 21:20:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let contagemepisodio = 1;
 let limiteepisodio = 5;
 let btPersonagem, btEpisodio;
 let TotalDePersonagens, TotalDeEpisodios;
+let Status
+let Species
+let Gender
 
 function criabt(container, functions, contagem, limite, total, tipo) {
     let bt = document.getElementById(`bt-carregar-${tipo.toLowerCase()}`);
@@ -34,15 +37,49 @@ async function caregarpersonagens() {
         TotalDePersonagens = response.info.count;
 
         while (contagempersonagem <= limitepersonagem && contagempersonagem <= TotalDePersonagens) {
+            Status = null
             let requestPersonagem = await fetch(`https://rickandmortyapi.com/api/character/${contagempersonagem}`);
             let responsePersonagem = await requestPersonagem.json();
             let div = document.createElement('div');
+            if(responsePersonagem.status === 'Alive' && responsePersonagem.gender === 'Male'){
+                Status = "Vivo"
+            }else if(responsePersonagem.status === 'Alive' && responsePersonagem.gender === 'Female'){
+                Status = "Viva"
+            }else if(responsePersonagem.status === 'Alive' && responsePersonagem.gender === 'unknown'){
+                Status = "Vivo"
+            }else if(responsePersonagem.status === 'unknown' && responsePersonagem.gender === 'Male'){
+                Status = "Desconhecido"
+            }else if(responsePersonagem.status === 'unknown' && responsePersonagem.gender === 'Female'){
+                Status = "Desconhecido"
+            }else if(responsePersonagem.status === 'Dead' && responsePersonagem.gender === 'Male'){
+                Status = "Morto"
+            }else if(responsePersonagem.status === 'Dead' && responsePersonagem.gender === 'Female'){
+                Status = "Morta"
+            }else if(responsePersonagem.status === 'Dead' && responsePersonagem.gender === 'unknown'){
+                Status = "Morto"
+            }
+
+            if(responsePersonagem.species === 'Human'){
+                Species = "Humana";
+            }else if(responsePersonagem.species === 'Alien'){
+                Species = "Alienigena"
+            }else if(responsePersonagem.species === 'unknown'){
+                Species = "Desconhecido"
+            }
+
+            if(responsePersonagem.gender === 'Female'){
+                Gender = "Feminino"
+            }else if(responsePersonagem.gender === 'Male'){
+                Gender = "Masculino"
+            }else if(responsePersonagem.gender === 'unknown'){
+                Gender = "Desconhecido"
+            }
             div.innerHTML = `
                 <img src="${responsePersonagem.image}" alt="${responsePersonagem.name}">
                 <p><strong>Nome:</strong> ${responsePersonagem.name}</p>
-                <p><strong>Status:</strong> ${responsePersonagem.status}</p>
-                <p><strong>Espécie:</strong> ${responsePersonagem.species}</p>
-                <p><strong>Gênero:</strong> ${responsePersonagem.gender}</p>
+                <p><strong>Status:</strong> ${Status}</p>
+                <p><strong>Espécie:</strong> ${Species}</p>
+                <p><strong>Gênero:</strong> ${Gender}</p>
             `;
             personagens.appendChild(div);
             contagempersonagem++;
